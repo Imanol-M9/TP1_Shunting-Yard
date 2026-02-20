ef2a72a11a01001a61b3b35a6233a1071d3a01db
Imanol <ptitranol@gmail.com>
2026-02-20 10:20:40 -0500

    Shunting Yard V0.0

---
+++ Shunting-yard.py
@@ -0,0 +1,10 @@
+def tokenize(expression: str) -> list[str]:
+    None
+
+
+def infix_to_postfix(tokens: list[str]) -> list[str]:
+    None
+
+
+def evaluate_postfix(tokens: list[str]) -> float:
+    None
