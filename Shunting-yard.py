56e3a728c2846e22e2ec3c5ea0caed01f3028435
Imanol <ptitranol@gmail.com>
2026-02-27 10:30:24 -0500

    Shunting Yard algoritme base: -evaluate_postfix fini -corection tokenize (dernier nombre n'etait pas integrer)

--- Shunting-yard.py
+++ Shunting-yard.py
@@ -7,6 +7,7 @@ def tokenize(expression: str) -> list[str]:
             element in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
             or element == "."
         ):
+            print(element)
             nombre_vergule += element
         elif (
             element in ["(", ")", "^", "*", "×", "/", "÷", "+", "−", "-"]
@@ -17,16 +18,15 @@ def tokenize(expression: str) -> list[str]:
             list_finale.append(element)
             nombre_vergule = str()
 
+    list_finale.append(float(nombre_vergule))
     return list_finale
 
 
 # Need to fin a solution for . and , in nomber
 def infix_to_postfix(tokens: list[str]) -> list[str]:
-    print(tokens)
     precedence_of_operator = {
         "(": 0,
         ")": 0,
-        "^": 4,
         "*": 3,
         "×": 3,
         "/": 3,
@@ -37,7 +37,6 @@ def infix_to_postfix(tokens: list[str]) -> list[str]:
     }
     output = []
     operators = []
-
     for token in tokens:
         if type(token) is type(str()) and token != "(" and token != ")":
             try:
@@ -48,10 +47,8 @@ def infix_to_postfix(tokens: list[str]) -> list[str]:
                 valeur_dernier_operatation < precedence_of_operator[token]
                 or token == "^"
             ):
-                print("ajout directe")
                 operators.append(token)
             elif valeur_dernier_operatation >= precedence_of_operator[token]:
-                print(f"vidage besoin de mettre {token}")
                 valeur_dernier_operatation = 0
                 for element in operators[::-1]:
                     if precedence_of_operator[token] <= precedence_of_operator[element]:
@@ -63,30 +60,54 @@ def infix_to_postfix(tokens: list[str]) -> list[str]:
             operators.append(token)
 
         elif token == ")":
-            print("----- fin de braket -----")
             operators_bracket = []
-            print("operators", operators)
             while operators[-1] != "(":
                 operators_bracket_temp = operators.pop(-1)
                 operators_bracket.append(operators_bracket_temp)
             del operators[-1]
-            print("opertaor apres", operators)
-            print("element a mettre", operators_bracket)
             for element in operators_bracket:
                 output.append(element)
-            # print(f"operateur apres supresion (){operators}")
 
         else:
             output.append(token)
-        print(f"output {output}, operators{operators}")
     for element in operators[::-1]:
         output.append(element)
-    print(f"ouput final {output}")
+    return output
 
 
 def evaluate_postfix(tokens: list[str]) -> float:
-    None
+    list_nombre = list()
+    precedence_of_operator = {
+        "(": 0,
+        ")": 0,
+        "*": 3,
+        "×": 3,
+        "/": 3,
+        "÷": 3,
+        "+": 2,
+        "−": 2,
+        "-": 2,
+    }
+    for token_index in range(len(tokens)):
+        if type(tokens[token_index]) is type(float()):
+            list_nombre.append(tokens[token_index])
+        if type(tokens[token_index]) is type(str()):
+            match precedence_of_operator[tokens[token_index]]:
+                case 2:
+                    if tokens[token_index] == "+":
+                        list_nombre.append(list_nombre[-2] + list_nombre[-1])
+                    elif tokens[token_index] == "-" or tokens[token_index] == "−":
+                        list_nombre.append(list_nombre[-2] - list_nombre[-1])
+                    del list_nombre[-2], list_nombre[-2]
+                case 3:
+                    if tokens[token_index] == "*" or tokens[token_index] == "×":
+                        list_nombre.append(list_nombre[-2] * list_nombre[-1])
+                    elif tokens[token_index] == "/" or tokens[token_index] == "÷":
+                        list_nombre.append(list_nombre[-2] / list_nombre[-1])
+                    del list_nombre[-2], list_nombre[-2]
+
+    return list_nombre[0]
 
 
-print(infix_to_postfix(tokenize("3 + 2.45 * 2 / ( 1 - 5 )")))
-print(infix_to_postfix(tokenize("3 + 2.45 * 2 / ( 1 - 5 )")) == 1)
+# print(infix_to_postfix(tokenize("3 + 2.45 * 2 / ( 1 - 5 )")))
+print(evaluate_postfix(infix_to_postfix(tokenize("((1+1+1)*3) / 9 "))))
